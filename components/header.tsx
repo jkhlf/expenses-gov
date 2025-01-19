257ca470de3b2786328b0f88cd3fc87413880bb4
Joao <joao.khalaf@gmail.com>
2025-01-19 17:30:03 -0300

    inicial charts by uf

---
+++ components/header.tsx
@@ -0,0 +1,29 @@
+import { Calendar, FlagIcon, Landmark, MapIcon } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+function header() {
+  return (
+     <header className='mb-12 flex justify-center items-center lg:flex-row flex-col gap-10 lg:justify-between'>
+        <div className='flex items-center gap-4'>  
+          <Landmark height={40} width={40}/>
+          <div className="flex flex-col ">
+            <h1 className='text-3xl font-bold mb-1'>Gastos dos Senadores</h1>
+            <p>Gastos dos senadores Total por estado (UF) - CEAPS</p>
+          </div>
+        </div>
+
+        <nav className='flex gap-6'>
+          <div className='border-r-2 pr-6 border-black/10 '>
+          <button className='flex flex-col items-center justify-center text-xs gap-2 border-black/10 border-2 px-4 py-3 rounded-lg'><Calendar/>Calendario</button></div>
+
+          <div className='flex gap-4'><button className='flex flex-col items-center justify-center text-xs gap-2 border-black/10 border-2 px-4 py-3 rounded-lg hover:border-violet-400'><MapIcon className='text-violet-400'/>Gastis por UF</button>
+          <button className='flex flex-col items-center justify-center text-xs gap-2 border-black/10 border-2 px-4 py-3 rounded-lg hover:border-violet-400'><FlagIcon/>Gastos por Partido</button>
+          </div>
+        </nav>
+      </header>
+     
+  )
+}
+
+export default header
---
+++ components/uf-chart.tsx
@@ -0,0 +1,58 @@
+'use client';
+import React, { JSX } from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
+import { ChartContainer, ChartTooltip } from './ui/chart'
+import { Bar, BarChart, LabelList, XAxis, YAxis } from 'recharts'
+
+type UfChartProps = {
+  year?: number,
+  data: {
+    year: string,
+    data: {uf:string, total_expenses:number}[]
+  } []
+}
+
+function CustomToolTip({ active, payload, label }: any): JSX.Element | null { 
+  if (active && payload && payload.length) {
+    return(
+      <div className='bg-white p-2 shadow-md rounded-md'>
+        <p className='label'>{`${label} : ${new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(payload[0].value)}`}</p>
+      </div>
+    )
+  }
+  return null;
+}
+
+function ufchart({data, year = 2024} : UfChartProps) {
+
+  let chartData =  data.find((item) => Number(item.year) === year)?.data
+  chartData= chartData?.sort((a,b) => b.total_expenses - a.total_expenses);
+
+  return (
+    <Card>
+      <CardHeader>
+          <CardTitle className='text-2xl'>Gastos Por UF</CardTitle>
+            <CardDescription> Dados de 2024</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <ChartContainer  config={{}} className='min-h-[900px] w-full'> 
+              <BarChart data={chartData} layout='vertical' >
+                <YAxis dataKey="uf" type='category' tickLine={false} axisLine={false} tickMargin={10} className='font-semibold'/>
+                <XAxis type='number' dataKey='total_expenses'/>
+                <ChartTooltip content={CustomToolTip}/>
+                <Bar dataKey="total_expenses"  fill="#8884d8" radius={4} layout='vertical'> 
+                  <LabelList
+                    dataKey={'total_expenses'}
+                    position={'insideRight'}
+                    fill={'#fff'}
+                    formatter={(value: any) => new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)}
+                  />
+                </Bar>
+              </BarChart>
+            </ChartContainer>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default ufchart
