c90738a011c452b0dd9208931cc66ad1546d64a6
Joao <joao.khalaf@gmail.com>
2025-01-20 17:28:36 -0300

    configure ESLint to ignore errors during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignorar erros de ESLint na build
+  },
 };
 
 export default nextConfig;
